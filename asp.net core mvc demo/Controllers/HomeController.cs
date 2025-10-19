9a9249b2ee742618f580b6506ec54dd3413f9568
omkar <omkar@DESKTOP-8KLE88J>
2025-10-19 15:41:08 +0530

    Add project files.

---
+++ asp.net core mvc demo/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using asp.net_core_mvc_demo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp.net_core_mvc_demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ asp.net core mvc demo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace asp.net_core_mvc_demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
